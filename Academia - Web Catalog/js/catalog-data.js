83d69dc663fbdd65c105cc5f05c7cf2dbb0e6b45
Pepez121 <jlmedinap12@gmail.com>
2017-08-10 16:54:08 -0500

    "Dynamic links added, it works but styles are broken"

---
+++ Academia - Web Catalog/js/catalog-data.js
@@ -0,0 +1,28 @@
+var myCatalog = {
+        "Product1": {
+            "id": 0,
+            "title": "Product 1",
+            "description": "fsion Product1 Description",
+            "imageurl": "img/products/hoodie1.jpg",
+            "model": "model1",
+            "price": "100"
+        },
+
+        "Product2": {
+            "id": 1,
+            "title": "Product 2",
+            "description": "Product2 Description Product2 DescriptionProduct2 Description",
+            "imageurl": "img/products/hoodie2.jpg",
+            "model": "model2",
+            "price": "200"
+        },
+
+        "Product3": {
+            "id": 2,
+            "title": "Product 3",
+            "description": "Product3 Description Product3 Description Product3 Description",
+            "imageurl": "img/products/hoodie3.jpg",
+            "model": "model3",
+            "price": "300"
+        }
+}
--- Academia - Web Catalog/js/catalog.js
+++ Academia - Web Catalog/js/catalog.js
@@ -1,26 +1,3 @@
-var myCatalog = {
-        "Product1": {
-            "id": 0,
-            "title": "Product 1",
-            "description": "fsion Product1 Description",
-            "imageurl": "img/products/hoodie1.jpg"
-        },
-
-        "Product2": {
-            "id": 1,
-            "title": "Product 2",
-            "description": "Product2 Description Product2 DescriptionProduct2 Description",
-            "imageurl": "img/products/hoodie2.jpg"
-        },
-
-        "Product3": {
-            "id": 2,
-            "title": "Product 3",
-            "description": "Product3 Description Product3 Description Product3 Description",
-            "imageurl": "img/products/hoodie3.jpg"
-        }
-}
-
 $(document).ready(function () {
     $("#prueba").append(buildMyProduct(myCatalog));
 
@@ -32,7 +9,8 @@ function buildMyProduct(obj) {
     $.each(obj, function(element, object){
         html += "<div class='col-xs-12 col-md-4'><div class='product-img'><img class='catalog-image' src='";
         html += object.imageurl + "'></div><div class='product-desc'><p class='prod-desc-title'>";
-        html += object.title + "</p><p class='prod-desc-text'>";
+        html += "<a href='product-detail.html?id=" + object.id + "'>";
+        html += object.title + "</a></p><p class='prod-desc-text'>";
         html += object.description + "</p></div></div></div>";
     });
     return html;
